4171ae4a9cabac699e5f1c3a3869470a5d648d2d
pervezmia <ahmedpervezkabir@gmail.com>
2026-08-07 21:31:12 +0600

    product insert and get

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 9000;
 console.log(port);
-const cors = require('cors')
-require('dotenv').config()
+const cors = require("cors");
+require("dotenv").config();
 
+app.use(cors());
+app.use(express.json());
 
-
-app.use(cors())
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.MONGO_DB_URI
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.MONGO_DB_URI;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -19,30 +17,46 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
 
-
     // await client.connect();
     const database = client.db("resells-hub");
     const productCollection = database.collection("products");
 
 
-    app.get("/products", async (req, res) => {
-        const cursor = productCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get("/api/product", async (req, res) => {
+      const query = {};
+      if(req.query.status){
+        query.status = req.query.status;
+      }
+      const cursor = await productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+
     })
 
+    app.post("/api/product", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+    });
 
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -50,16 +64,13 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-//user : resells-hub 
+//user : resells-hub
 //password: yDheCeQv0X6R31YM
