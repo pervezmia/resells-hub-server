fe6cbd04a25dc5f900f35b01cf4b8efb8c8a6c42
pervezmia <ahmedpervezkabir@gmail.com>
2026-08-14 06:05:48 +0600

    vercel.json added

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
-  try {
+// async function run() {
+//   try {
     // Connect the client to the server	(optional starting in v4.7)
 
     // await client.connect();
@@ -360,15 +360,15 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+  //   await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
+  // } finally {
+  //   // Ensures that the client will close when you finish/error
+  //   // await client.close();
+  // }
+// }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
@@ -379,5 +379,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
 
-//user : resells-hub
-//password: yDheCeQv0X6R31YM
+
